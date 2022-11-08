868304caa49c3d38dbb0c12ef3572913d73f9345
sreemaye <sreemaye44@gmail.com>
2022-11-08 23:41:34 +0600

    added review api methods

---
+++ index.js
@@ -0,0 +1,96 @@
+const express= require('express');
+const cors= require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app =express();
+const port=process.env.PORT||5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json())
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ewurel7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+   try{
+      const serviceCollection=client.db('photography').collection('photography-area');
+      const reviewCollection=client.db('photography').collection('review-area');
+
+      app.get('/services', async(req,res)=>{
+           const query=parseInt(req.query?.total == undefined ? 0: req.query?.total );
+           const cursor=serviceCollection.find({}).sort({"creationDate":-1}).limit(query);
+           const services=await cursor.toArray();
+           res.send(services)
+
+      });
+      app.get('/allServices',async(req,res)=>{
+           const query={};
+           const cursor=serviceCollection.find(query);
+           const allServices=await cursor.toArray();
+           res.send(allServices)
+
+      });
+
+      app.get('/services/:id', async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)}
+        const service=await serviceCollection.findOne(query);
+        res.send(service);
+
+});
+    app.get('/review', async(req,res)=>{
+    let query={};
+    if(req.query.service){
+        query=
+        {
+            service: req.query.service
+        }
+    }
+    const cursor=reviewCollection.find(query).sort({"creationDate":-1});
+        const review=await cursor.toArray();
+        res.send(review);
+      });
+    
+    app.post('/services', async(req,res)=>{
+    const newService=req.body;
+    newService.creationDate = new Date();
+    const service=await serviceCollection.insertOne(newService);
+    res.send(service);
+
+});
+    app.post('/review', async(req,res)=>{
+    const newReview=req.body;
+    newReview.creationDate = new Date();
+    const review=await reviewCollection.insertOne(newReview);
+    res.send(review);
+
+});
+
+   
+
+
+   }
+
+   finally{
+
+   }
+}
+
+run().catch(err=>console.error(err))
+
+
+app.get('/',(req,res)=>{
+    res.send('genius car server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`genius car server running on,${port}`)
+})
